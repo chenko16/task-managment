c1bc86640050a72b75e0bc3c8ef9acb0d423ec8a
kir <timofeev.log@narod.ru>
2020-12-10 22:05:06 +0300

    add user role claim and fix data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,2 +1,3 @@
 insert into security_user ("user_id", "active", "login", "password", "system_role")
-values (1, true, 'admin','$2a$10$cWuqqF5j7BS2VB2TPExBTeBDKe9uWjl6cWEfWnrt20vRJqhQaYzi.', 'ADMIN');
+values (1, true, 'admin','$2a$10$cWuqqF5j7BS2VB2TPExBTeBDKe9uWjl6cWEfWnrt20vRJqhQaYzi.', 'ADMIN')
+on conflict ("user_id") do nothing;
